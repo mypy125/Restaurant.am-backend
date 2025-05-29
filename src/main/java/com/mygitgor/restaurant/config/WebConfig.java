b877bb6d84219da6106e14274eaf7c39dba70c87
mohito <mypywelcome@gmail.com>
2025-05-29 17:12:23 +0400

    added_resolving

--- src/main/java/com/mygitgor/restaurant/config/WebConfig.java
+++ src/main/java/com/mygitgor/restaurant/config/WebConfig.java
@@ -14,8 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("https://mypy125.github.io/")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
